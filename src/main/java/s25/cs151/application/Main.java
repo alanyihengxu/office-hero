1d4bd4787185a4fe7d67f4867dacd7fb8dd3d0a5
Benny Wen <bennywen0.0@gmail.com>
2025-03-14 20:30:44 -0700

    office-hour-broken-code

--- src/main/java/s25/cs151/application/Main.java
+++ src/main/java/s25/cs151/application/Main.java
@@ -1,38 +1,27 @@
 package s25.cs151.application;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+import java.nio.file.DirectoryNotEmptyException;
+import java.util.Stack;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), Constants.WIDTH, Constants.HEIGHT);
-
-        stage.setTitle("Office Hero");
-
-        stage.setScene(scene);
-
-        stage.show();
-
-        //temporary code
         Pane root = new Pane();
         StackPane stackPane = new StackPane();
         StackPane stackPane2 = new StackPane();
@@ -162,15 +151,13 @@ public class Main extends Application {
         root.getChildren().add(logoText);
         root.getChildren().add(submit);
 
-        scene = new Scene(root, 1000, 600);
+        Scene scene = new Scene(root, 1000, 600);
         stage.setScene(scene);
         stage.setTitle("Office-hero");
         stage.show();
-
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
-
 }
